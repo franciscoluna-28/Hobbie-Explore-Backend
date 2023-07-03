7345dcbc741c175ecf8425eea3033108c0d60385
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-07-03 10:02:38 -0400

    Created types for boredAPI

--- src/types/boredAPITypes.ts
+++ src/types/boredAPITypes.ts
@@ -1,13 +1,13 @@
 // Without changing the key parameter to id
 export interface DefaultBoredAPIActivity {
-    activity: string;
-    type: string;
-    participants: number;
-    price: number;
-    link: string;
-    key: string;
-    accessibility: number;
-  }
+  activity: string;
+  type: string;
+  participants: number;
+  price: number;
+  link: string;
+  key: string;
+  accessibility: number;
+}
 
 // After processing the key and using it as the ID
 export interface BoredAPIModifiedActivity {
@@ -20,5 +20,15 @@ export interface BoredAPIModifiedActivity {
   accessibility: number;
 }
 
-
-  
+// Available categories for Bored API
+export enum BoredAPIActivityType {
+  Education = "education",
+  Recreational = "recreational",
+  Social = "social",
+  DIY = "diy",
+  Charity = "charity",
+  Cooking = "cooking",
+  Relaxation = "relaxation",
+  Music = "music",
+  Busywork = "busywork",
+}
