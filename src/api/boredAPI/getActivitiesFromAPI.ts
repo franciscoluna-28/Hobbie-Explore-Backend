1ee5fdf952682d4f2895ed4b724ab37785fa3c1b
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-07-03 16:22:08 -0400

    Splitted code and logic into sub - functions

--- src/api/boredAPI/getActivitiesFromAPI.ts
+++ src/api/boredAPI/getActivitiesFromAPI.ts
@@ -1,16 +1,11 @@
+import { BoredAPIActivityType } from "./../../types/boredAPITypes";
 import axios from "axios";
 import { boredAPIAvailableUrls } from "./urlDeclarations";
 import {
-  BoredAPIActivityType,
   BoredAPIModifiedActivity,
   DefaultBoredAPIActivity,
 } from "../../types/boredAPITypes";
-import { generateArray } from "../../helpers/generateArray";
 import { AxiosResponse } from "axios";
-
-//TODO Remove unused logic
-// TODO Figure out a better way of retrieving activities in group
-// TODO To be honest I don't want to use for loops this time...
 
 // Helper function to transform "key" to "id" to avoid React conflicts
 function transformBoredAPIKeyToId(
@@ -20,50 +15,16 @@ function transformBoredAPIKeyToId(
   return { ...modifiedActivity, id: key };
 }
 
-/* // Getting some random activities
-export async function getRandomActivities(count: number) {
-  if (count === 1) {
-    const response = await axios.get(boredAPIAvailableUrls.randomActivityUrl);
-    return transformBoredAPIKeyToId(response);
-  }
-
-  const activities = await Promise.all(
-    generateArray(count, {}).map(() =>
-      axios
-        .get(boredAPIAvailableUrls.randomActivityUrl)
-        .then((response) => transformBoredAPIKeyToId(response))
-    )
-  );
-
-  return activities;
-} */
-
-// TODO add Axios types here
-export async function getOneRandomActivity(){
+export async function getOneRandomActivity() {
   const response = await axios.get(boredAPIAvailableUrls.randomActivityUrl);
   return transformBoredAPIKeyToId(response);
 }
 
-/* // Getting different activities according to a type
-export async function getRandomActivitiesByType(
-  count: number,
-  type: BoredAPIActivityType
+export async function getOneRandomActivityWithQuery(
+  query: BoredAPIActivityType
 ) {
-  if (count === 1) {
-    const response = await axios.get(
-      boredAPIAvailableUrls.randomActivityWithTypeUrl + type
-    );
-    return transformBoredAPIKeyToId(response);
-  }
-
-  const activities = await Promise.all(
-    generateArray(count, {}).map(() =>
-      axios
-        .get(boredAPIAvailableUrls.randomActivityWithTypeUrl + type)
-        .then((response) => transformBoredAPIKeyToId(response))
-    )
+  const response = await axios.get(
+    boredAPIAvailableUrls.randomActivityUrl + query
   );
-
-  return activities;
+  return transformBoredAPIKeyToId(response);
 }
- */
