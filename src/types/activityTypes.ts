f8d7307650a7dfb3a71b38e2abe931122db2a7f9
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-07-03 16:23:21 -0400

    Changed final types for the activity

--- src/types/activityTypes.ts
+++ src/types/activityTypes.ts
@@ -1,7 +1,8 @@
 import { BoredAPIModifiedActivity } from "./boredAPITypes";
-import { UnsplashImage } from "./unsplashAPITypes";
+import { ProcessedUnsplashImage } from "./unsplashAPITypes";
 
 // Type to be used by the final API
 export interface ActivityWithImage extends BoredAPIModifiedActivity {
-  image?: UnsplashImage;
+  image?: ProcessedUnsplashImage;
 }
+
