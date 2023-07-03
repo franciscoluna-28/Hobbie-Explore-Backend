bd06eaace52f365ee183b712948a29a832a875ef
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-07-03 10:01:52 -0400

    Created Unsplash Urls

---
+++ src/api/unsplashImage/urlDeclarations.ts
@@ -0,0 +1,16 @@
+interface UnsplashAvailableUrls {
+  getImageWithQuery: string;
+}
+
+const unsplashAccessKey = process.env.UNSPLASH_ACCESS_KEY!;
+
+// Unsplash API endpoints
+export const unsplashAPIAvailableUrls: UnsplashAvailableUrls = {
+  getImageWithQuery:
+    "https://api.unsplash.com/search/photos?client_id=" +
+    unsplashAccessKey +
+    "&per_page=1" +
+    "&page=3" +
+    "&query=",
+};
+
