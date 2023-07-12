c3a1359f2c923e0a14cf3d9a476452c9a1b4c014
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-07-12 17:16:20 -0400

    add api routes

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,34 @@
-import express from "express";
+import express, { Application, Router } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import activityRouter from "./api/activity/activityRouter";
+import Middleware from "./middlewares/firebaseValidation";
+import userRouter from "./api/user/userRouter";
 
 // Starting the application
-const app = express();
+const app: Application = express();
 
 // Proper middleware setup
 dotenv.config();
-app.use(cors());
 app.use(express.json());
+app.use(cors());
+
+const authMiddlewareFirebase = new Middleware();
+
+app.use(authMiddlewareFirebase.decodeToken);
+
+// Create the main API router
+const apiRouter: Router = Router();
+
+// Mount the user router without the middleware
+apiRouter.use("/user", userRouter);
+
+// Apply the middleware only to the activity routes
+
+// Mounting additional routers in our API
+apiRouter.use("/activity", activityRouter);
+
+// Application is under /api path
+app.use("/api", apiRouter);
 
 export default app;
