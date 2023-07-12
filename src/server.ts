cbdf15280e5b7bd0b1ada791370ea42c135523cb
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-07-12 17:16:06 -0400

    remove old functions of db from index

--- src/server.ts
+++ src/server.ts
@@ -1,47 +1,15 @@
 import app from ".";
 import { ActivityRepository } from "./api/activity/activityRepository";
-
 const port = process.env.SERVER_PORT;
+import { connectToDatabase } from "./db";
 
-
-
-
-
-
-
-
-
-
-//TODO connect to database
 // Starting the server
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
-});
-
-const activityRepository = new ActivityRepository();
-activityRepository.getOneRandomActivity().then((activity) => {
-  console.log(activity);
-}).catch((error) => {
-  console.log(error);
-});
-
-/* async function getRandomActivity() {
-  try {
-    const data = await getOneRandomActivity();
-    return data;
-  } catch (error) {
-    console.error(error);
-    throw error;
-  }
-}
-
-(async () => {
-  try {
-    const y = await getRandomActivity();
-    getBestKeyword(y);
-    console.table(y);
-  } catch (error) {
-    console.error(error);
-  }
-})(); */
-
+connectToDatabase()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Server listening at http://localhost:${port}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Error connecting to the database:", error);
+  });
