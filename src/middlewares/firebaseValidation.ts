e57a81f63c86dd3afe987c7ee4ebd796c6f03b1b
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-07-12 17:11:02 -0400

    create firebase validation middleware

---
+++ src/middlewares/firebaseValidation.ts
@@ -0,0 +1,29 @@
+import { admin } from "../config/firebase-config";
+import { NextFunction, Request, Response } from "express";
+
+// Middleware for Firebase 
+class FirebaseMiddleware {
+  async decodeToken(req: Request, res: Response, next: NextFunction) {
+    const token = req.headers.authorization?.split(" ")[1];
+
+    if (!token) {
+      // No se proporcionó ningún token en el encabezado de autorización
+      return res.status(401).json({ message: "Unauthorized! Token missing" });
+    }
+
+    try {
+      const decodeValue = await admin.auth().verifyIdToken(token);
+
+      if (decodeValue) {
+        return next();
+      }
+
+      return res.status(401).json({ message: "Unauthorized! Invalid token" });
+    } catch (error) {
+      console.error("Error decoding token:", error);
+      return res.status(500).json({ message: "Internal error" });
+    }
+  }
+}
+
+export default FirebaseMiddleware;
