904e1a0b547be1b6304f0b5bbbf355d46dbde372
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-07-12 17:07:25 -0400

    update boredAPI types

--- src/types/activityTypes.ts
+++ src/types/activityTypes.ts
@@ -1,4 +1,4 @@
-import { BoredAPIModifiedActivity } from "./boredAPITypes";
+import { BoredAPIActivityType, BoredAPIModifiedActivity } from "./boredAPITypes";
 import { ProcessedUnsplashImage } from "./unsplashAPITypes";
 
 // Type to be used by the final API
@@ -6,3 +6,28 @@ export interface ActivityWithImage extends BoredAPIModifiedActivity {
   image?: ProcessedUnsplashImage;
 }
 
+// Final API reponse to be consumed by the Frontend
+export interface HobbieExploreActivityWihtImage {
+  activity: string;
+  type: BoredAPIActivityType | string;
+  participants: number;
+  price: number;
+  link: string;
+  accessibility: number;
+  id: string;
+  urls: {
+    full: string;
+  };
+  user: {
+    name: string;
+    username: string;
+    links: {
+      html: string;
+    };
+    profile_image: {
+      medium: string;
+    };
+  };
+}
+
+export type HobbieExploreActivityIDArray = { id: string };
---
+++ src/types/userTypes.ts
@@ -0,0 +1,30 @@
+
+import { Document } from "mongodb";
+import { HobbieExploreActivityIDArray } from "./activityTypes";
+
+// User to be inserted into the database
+export interface HobbieExploreUser extends Document {
+    uid: string;
+    email?: string | null;
+    emailVerified: boolean;
+    displayName?: string | null;
+    photoURL?: string | null;
+    phoneNumber?: string | null;
+    disabled: boolean;
+    bearedToken: string;
+    metadata: {
+      creationTime?: string | null;
+      lastSignInTime?: string | null;
+    };
+    providerData: Array<{
+      uid: string;
+      displayName?: string | null;
+      email?: string | null;
+      photoURL?: string | null;
+      phoneNumber?: string | null;
+      providerId: string;
+    }>;
+    savedHobbiesAndActivities: HobbieExploreActivityIDArray[]
+  }
+  
+
