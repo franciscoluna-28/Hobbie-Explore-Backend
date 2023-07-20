86ee5b25c45e2ac8a8c14b77b51fa529a475bcd4
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-07-19 20:06:15 -0400

    remove link and change act to actname

--- src/types/activityTypes.ts
+++ src/types/activityTypes.ts
@@ -1,18 +1,17 @@
-import { BoredAPIActivityType, BoredAPIModifiedActivity } from "./boredAPITypes";
+import { BoredAPIActivityType, BoredAPIModifiedActivity, ProcessedBoredAPIModifiedActivity } from "./boredAPITypes";
 import { ProcessedUnsplashImage } from "./unsplashAPITypes";
 
 // Type to be used by the final API
-export interface ActivityWithImage extends BoredAPIModifiedActivity {
+export interface ActivityWithImage extends ProcessedBoredAPIModifiedActivity {
   image?: ProcessedUnsplashImage;
 }
 
 // Final API reponse to be consumed by the Frontend
 export interface HobbieExploreActivityWihtImage {
-  activity: string;
+  activityName: string;
   type: BoredAPIActivityType | string;
   participants: number;
   price: number;
-  link: string;
   accessibility: number;
   activityId: string;
   urls: {
