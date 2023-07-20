77eada6e949d1c43fd5ce27b2e1de6a5648c28e9
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-07-19 20:05:56 -0400

    add functionality to search activities with query

--- src/api/activity/activityRouter.ts
+++ src/api/activity/activityRouter.ts
@@ -5,8 +5,10 @@ const activityRouter = Router();
 const activityController = new ActivityController();
 
 activityRouter.get("/random", activityController.getThreeRandomActivities);
-activityRouter.get("/random-with-query/:type", activityController.getThreeRandomActivitiesWithQuery);
-activityRouter.get("/search/:query", activityController.searchActivities);
+activityRouter.get(
+  "/random-with-query/:type",
+  activityController.getThreeRandomActivitiesWithQuery
+);
+activityRouter.get("/search", activityController.searchActivities);
 
 export default activityRouter;
-
