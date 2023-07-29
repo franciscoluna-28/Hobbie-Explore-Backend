287415395c1457e184f7c745a66dc03b02a1bf0c
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-07-28 21:28:49 -0400

    change db implementation to mongoose

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,17 @@
 import app from ".";
-import { ActivityRepository } from "./api/activity/activityRepository";
 const port = process.env.SERVER_PORT;
-import { connectToDatabase } from "./db";
+import { db } from "./db"; // Importar la conexión a la base de datos desde el archivo db.ts
+import { updateActivitiesWithDescription } from "./utils/addDescriptionAndLinksToActivities";
 
 // Starting the server
-connectToDatabase()
-  .then(() => {
-    app.listen(port, () => {
-      console.log(`Server listening at http://localhost:${port}`);
-    });
-  })
-  .catch((error) => {
-    console.error("Error connecting to the database:", error);
+db.once("open", () => {
+  console.log(`Successfully connected to database: ${db.name}`);
+  app.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`);
+    updateActivitiesWithDescription();
   });
+});
+
+db.on("error", (error) => {
+  console.error("Error connecting to the database:", error);
+});
