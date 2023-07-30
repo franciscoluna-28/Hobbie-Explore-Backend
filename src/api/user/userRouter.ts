30c9d71483e3714f821417af5ee0b6bb9cc49592
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-07-30 17:45:38 -0400

    format user router

--- src/api/user/userRouter.ts
+++ src/api/user/userRouter.ts
@@ -7,8 +7,27 @@ const routerController = new UserController();
 
 userRouter.post("/register-user", routerController.registerUser);
 userRouter.post("/register-user-token/:uid", routerController.addTokenToUser);
-userRouter.post("/add-activity-to-user/:uid", routerController.addActivityToUser);
-userRouter.delete("/remove-activity-from-user/:uid", routerController.deleteActivityFromUser);
-userRouter.get("/get-activities-by-user/:uid", routerController.getCurrentUserActivities)
+userRouter.post(
+  "/add-activity-to-user/:uid",
+  routerController.addActivityToUser
+);
+userRouter.delete(
+  "/remove-activity-from-user/:uid",
+  routerController.deleteActivityFromUser
+);
+userRouter.get(
+  "/get-activities-by-user/:uid",
+  routerController.getCurrentUserActivities
+);
+userRouter.delete("/delete-user/:uid", routerController.deleteUser);
+/* userRouter.post("/rate-activity", routerController.rateActivity);
+userRouter.post(
+  "/get-rating-in-activity",
+  routerController.getCurrentUserRating
+); */
+/* userRouter.post("/comment-activity", routerController.addCommentToActivity); */
+/* userRouter.post("/get-activity-comments", routerController.getActivityComments)
+userRouter.post("/edit-comment", routerController.editComment) */
+
 
 export default userRouter;
