691e6c1180f99f9a49a431011b903b257158dbe2
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-08-06 17:38:17 -0400

    create comment route

--- src/api/comment/commentRouter.ts
+++ src/api/comment/commentRouter.ts
@@ -1,11 +1,21 @@
 import { Router } from "express";
 import { CommentController } from "./commentController";
-const commentRouter =  Router();
+const commentRouter = Router();
 
-const commentController = new CommentController()
+const commentController = new CommentController();
 
-commentRouter.post("/add-comment-in-activity", commentController.addNewComment)
-commentRouter.get("/get-comments-from-activity/:activityId", commentController.getAllCommentsFromActivity);
-
+commentRouter.post("/add-comment-in-activity", commentController.addNewComment);
+commentRouter.get(
+  "/get-comments-from-activity/:activityId",
+  commentController.getAllCommentsFromActivity
+);
+commentRouter.delete(
+  "/remove-comment-from-activity/:uid/:activityId/:commentId",
+  commentController.removeCommentFromActivity
+);
+commentRouter.post(
+  "/edit-comment-from-activity/:uid/:activityId/:commentId",
+  commentController.editComment
+);
 
 export default commentRouter;
