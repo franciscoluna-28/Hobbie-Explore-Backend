b0eda3475af027a36de8050f760c46cb097f02be
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-08-08 16:45:04 -0400

    change url orientation for the retrieved images

--- src/api/unsplashImage/urlDeclarations.ts
+++ src/api/unsplashImage/urlDeclarations.ts
@@ -1,4 +1,5 @@
 import dotenv from "dotenv";
+import { unsplashOrientationList } from "./orientationList";
 
 dotenv.config();
 
@@ -6,14 +7,14 @@ interface UnsplashAvailableUrls {
   getImageWithQuery: string;
 }
 
-const unsplashAccessKey = process.env.UNSPLASH_ACCESS_KEY!;
+const unsplashAccessKey = process.env.UNSPLASH_ACCESS_KEY;
 
-// Unsplash API endpoints
 export const unsplashAPIAvailableUrls: UnsplashAvailableUrls = {
   getImageWithQuery:
     "https://api.unsplash.com/search/photos?client_id=" +
     unsplashAccessKey +
     "&per_page=1" +
+    "&orientation=" + unsplashOrientationList.landscape +
     "&page=3" +
     "&query=",
 };
