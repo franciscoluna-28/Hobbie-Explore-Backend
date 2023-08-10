fc81e2631ae2f3ac61efa6514e2a0c541d03a3a4
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-08-10 17:23:56 -0400

    create userTokenRepository

---
+++ src/api/user/repository/userTokenRepository.ts
@@ -0,0 +1,26 @@
+import { Model } from "mongoose";
+import { IUser } from "../../../types/userTypes";
+import { findUserByUid } from "../../../helpers/userRepositoryHelper";
+
+export class UserTokenRepository {
+  private userModel: Model<IUser>;
+
+  constructor(userModel: Model<IUser>) {
+    this.userModel = userModel;
+  }
+
+  async giveTokenToUser(uid: string, newToken: string) {
+    const currentUser = await findUserByUid(this.userModel, uid);
+
+    if (!currentUser) {
+      return { error: "User not found" };
+    }
+
+    await this.userModel.findOneAndUpdate(
+      { uid: uid },
+      { $set: { bearedToken: newToken } }
+    );
+
+    return { success: true };
+  }
+}
