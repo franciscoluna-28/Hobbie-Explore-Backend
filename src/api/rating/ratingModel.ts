98ab907084401fc3f686b3c54c756846ad5ac72d
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-08-13 16:44:44 -0400

    create ratingModel

---
+++ src/api/rating/ratingModel.ts
@@ -0,0 +1,31 @@
+import { Schema, Document, model, Types, ObjectId } from "mongoose";
+
+// Interfaz para el modelo de ratings
+export interface IUserRating extends Document {
+  userId: Schema.Types.ObjectId,
+  rating: number;
+  activityId: string,
+}
+
+const ratingSchema = new Schema<IUserRating>({
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  rating: {
+    type: Number,
+    required: true,
+    min: 1,
+    max: 5
+  },
+  activityId: {
+    type: String,
+    required: true,
+  }
+});
+
+// Modelo para el rating
+const RatingModel = model<IUserRating>("Rating", ratingSchema);
+
+export default RatingModel;
