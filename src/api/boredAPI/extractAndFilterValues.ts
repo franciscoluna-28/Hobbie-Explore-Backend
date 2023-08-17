b2d9a1d13be59ba430502d4f655321264383500f
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-08-17 17:41:59 -0400

    add id and change activityName to name

--- src/api/boredAPI/extractAndFilterValues.ts
+++ src/api/boredAPI/extractAndFilterValues.ts
@@ -3,13 +3,13 @@ import { BoredAPIModifiedActivity } from "../../types/boredAPITypes";
 // Function to remove the field of "link" because is empty 99% of the times
 export function extractLinkFromBored(activity: BoredAPIModifiedActivity) {
   const {
-    activity: activityName,
+    name,
     type,
     participants,
     price,
-    activityId,
     accessibility,
+    id,
   } = activity;
 
-  return { activityName, type, participants, price, activityId, accessibility };
+  return { name, type, participants, price, accessibility, id };
 }
