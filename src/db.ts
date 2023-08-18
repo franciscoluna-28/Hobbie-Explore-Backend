0bbca21cc4a3bccd767f8f521e1d6b1ba83cba9a
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-08-18 11:52:51 -0400

    .

--- src/db.ts
+++ src/db.ts
@@ -1,5 +1,7 @@
 import mongoose from "mongoose";
 
+
+console.log("WORK PLEASE DB URL: ", process.env.DATABASE_URI)
 // Mongoose Connection
 mongoose.connect(process.env.DATABASE_URI!, {
   dbName: process.env.DB_NAME,
