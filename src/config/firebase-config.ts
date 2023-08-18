3e1120471ae37d0473dd2036de694192f48077e1
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-08-18 09:47:10 -0400

    create firebase config file

---
+++ src/config/firebase-config.ts
@@ -0,0 +1,24 @@
+import dotenv from "dotenv";
+import admin, { ServiceAccount } from "firebase-admin";
+
+
+// Path to service account
+const serviceAccount: ServiceAccount | any = {
+  "type": process.env.TYPE,
+  "project_id": process.env.PROJECT_ID,
+  "private_key_id": process.env.PRIVATE_KEY_ID,
+  "private_key": process.env.PRIVATE_KEY,
+  "client_email": process.env.CLIENT_EMAIL,
+  "auth_uri": process.env.AUTH_URI,
+  "token_uri": process.env.TOKEN_URI,
+  "auth_provider_x509_cert_url": process.env.AUTH_PROVIDER_X509_CERT_URL,
+  "client_x509_cert_url": process.env.CLIENT_X509_CERT_URL,
+  "universe_domain": process.env.UNIVERSE_DOMAIN,
+};
+
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+export default admin;
