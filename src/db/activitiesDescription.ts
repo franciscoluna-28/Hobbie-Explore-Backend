cffefa5f617b61c193d610350187448bae09c088
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-08-18 09:47:46 -0400

    create placeholder for description of activities

---
+++ src/db/activitiesDescription.ts
@@ -0,0 +1,63 @@
+export const activitiesDescription = [
+  {
+    name: "Start a daily journal",
+    idToAdd: "8779876",
+    description:
+      "Embark on a beautiful journey of self-discovery and reflection by starting a daily journal. Take a few minutes each day to put pen to paper and record your thoughts, emotions, and experiences. Journaling can be a therapeutic and mindful practice, allowing you to gain deeper insights into your life and feelings. You can write about your goals, dreams, challenges, and successes, creating a personal narrative that captures your growth over time. To help you get started, consider using apps like Day One or Journey for digital journaling, or opt for a classic notebook like the Moleskine Journal. Make journaling a daily ritual that nurtures your mental well-being and preserves cherished memories for years to come.",
+    links: [
+      {
+        Notion: "https://www.notion.so/product",
+        Dayone: "https://dayoneapp.com/blog/journal-writing/",
+      },
+    ],
+  },
+  {
+    name: "Play a game of tennis with a friend",
+    idToAdd: "1093640",
+    description:
+      "Enjoy a fun and energetic game of tennis with a friend. Tennis is a fantastic sport that offers great exercise, friendly competition, and lots of enjoyment. Whether you are a seasoned player or a beginner, hitting the tennis ball back and forth on the court is a great way to improve your hand-eye coordination, agility, and stamina. It's a social and active activity that allows you to bond with your friend while engaging in a friendly match. Don't forget to bring your tennis racket, tennis balls, and lots of enthusiasm for an exciting game of tennis!",
+    links: [{
+      "Tennis Rules":
+        "https://www.usta.com/en/home/about-usta/who-we-are/national/usta-official-rules-of-tennis.html",
+      "Tennis Court Finder": "https://www.tennisround.com/tennis-courts",
+    }],
+  },
+  {
+    name: "Play a game of tennis with a friend",
+    idToAdd: "8125168",
+    description:
+      "Enjoy a fun and energetic game of tennis with a friend. Tennis is a fantastic sport that offers great exercise, friendly competition, and lots of enjoyment. Whether you are a seasoned player or a beginner, hitting the tennis ball back and forth on the court is a great way to improve your hand-eye coordination, agility, and stamina. It's a social and active activity that allows you to bond with your friend while engaging in a friendly match. Don't forget to bring your tennis racket, tennis balls, and lots of enthusiasm for an exciting game of tennis!",
+    links: [{
+      "Tennis Rules":
+        "https://www.usta.com/en/home/about-usta/who-we-are/national/usta-official-rules-of-tennis.html",
+      "Tennis Court Finder": "https://www.tennisround.com/tennis-courts",
+    }],
+  },
+  {
+    name: "Play a game of tennis with a friend",
+    idToAdd: "8125168",
+    description:
+      "Enjoy a fun and energetic game of tennis with a friend. Tennis is a fantastic sport that offers great exercise, friendly competition, and lots of enjoyment. Whether you are a seasoned player or a beginner, hitting the tennis ball back and forth on the court is a great way to improve your hand-eye coordination, agility, and stamina. It's a social and active activity that allows you to bond with your friend while engaging in a friendly match. Don't forget to bring your tennis racket, tennis balls, and lots of enthusiasm for an exciting game of tennis!",
+    links: [{
+      "Tennis Rules":
+        "https://www.usta.com/en/home/about-usta/who-we-are/national/usta-official-rules-of-tennis.html",
+      "Tennis Court Finder": "https://www.tennisround.com/tennis-courts",
+    }],
+  },
+  {
+    idToAdd: "8394738",
+    name: "Learn origami",
+    description: "aaaaaaaaaaaaaaaaaaaaaaaaaaa",
+    links: [{
+      "abc": "www.googlge.com"
+    }]
+  },
+  {
+    idToAdd: "4286250",
+    name: "Go for a walk",
+    description: "aaaaaaaaaaaaaaaaaaaaaaaaaaa",
+    links: [{
+      "abc": "www.google.com"
+    }]
+  }
+];
