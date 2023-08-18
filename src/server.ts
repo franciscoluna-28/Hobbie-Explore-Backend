5e4fd0f790777b60d67ad9c46a2a4cf413981c4c
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-08-18 12:23:41 -0400

    production bug fix

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import app from ".";
-const port = process.env.SERVER_PORT;
+const port = process.env.SERVER_PORT || 3000;
 import { db } from "./db"; // Importar la conexión a la base de datos desde el archivo db.ts
 import { updateActivitiesWithDescription } from "./utils/addDescriptionAndLinksToActivities";
 
@@ -7,7 +7,7 @@ import { updateActivitiesWithDescription } from "./utils/addDescriptionAndLinksT
 db.once("open", () => {
   console.log(`Successfully connected to database: ${db.name}`);
   app.listen(port, () => {
-    console.log(`Server listening at http://localhost:${port}`);
+    console.log(`Server listening at port ${port}`);
     updateActivitiesWithDescription();
   });
 });
