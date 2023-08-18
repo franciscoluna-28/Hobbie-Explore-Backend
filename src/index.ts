8d2247466f73eb912d92f8b6781c9d7fa1c986f9
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-08-18 09:47:29 -0400

    add comment router

--- src/index.ts
+++ src/index.ts
@@ -4,29 +4,29 @@ import dotenv from "dotenv";
 import activityRouter from "./api/activity/activityRouter";
 import Middleware from "./middlewares/firebaseValidation";
 import userRouter from "./api/user/userRouter";
+import ratingRouter from "./api/rating/ratingRouter";
+import commentRouter from "./api/comment/commentRouter";
 
 // Starting the application
 const app: Application = express();
+
+const firebaseMiddleware = new Middleware(); 
 
 // Proper middleware setup
 dotenv.config();
 app.use(express.json());
 app.use(cors());
+/* app.use(firebaseMiddleware.decodeToken) */
 
-const authMiddlewareFirebase = new Middleware();
-
-app.use(authMiddlewareFirebase.decodeToken);
 
 // Create the main API router
 const apiRouter: Router = Router();
 
-// Mount the user router without the middleware
+// Sub - routes
 apiRouter.use("/user", userRouter);
-
-// Apply the middleware only to the activity routes
-
-// Mounting additional routers in our API
+apiRouter.use("/rating", ratingRouter);
 apiRouter.use("/activity", activityRouter);
+apiRouter.use("/comment", commentRouter);
 
 // Application is under /api path
 app.use("/api", apiRouter);
