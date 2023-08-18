ff8d591b3a1fe2c92b1a7f494114050498df7798
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-08-18 17:45:49 -0400

    add singleton and unique global instance

--- src/api/user/repository/userRepository.ts
+++ src/api/user/repository/userRepository.ts
@@ -1,43 +1,57 @@
-    import { UserActionsRepository } from "./userActionsRepository";
-    import { UserActivityRepository } from "./userActivityRepository";
-    import { UserDescriptionRepository } from "./userDescriptionRepository";
-    import { UserStatsRepository } from "./userStatsRepository";
-    import { UserTokenRepository } from "./userTokenRepository";
-    import { Model } from "mongoose";
-    import { IUser } from "../../../types/userTypes";
+import { UserActionsRepository } from "./userActionsRepository";
+import { UserActivityRepository } from "./userActivityRepository";
+import { UserDescriptionRepository } from "./userDescriptionRepository";
+import { UserStatsRepository } from "./userStatsRepository";
+import { UserTokenRepository } from "./userTokenRepository";
+import { Model } from "mongoose";
+import { IUser } from "../../../types/userTypes";
+import UserModel from "../userModel";
 
-    export class UserRepository {
-    private userActionsRepository: UserActionsRepository;
-    private userActivityRepository: UserActivityRepository;
-    private userDescriptionRepository: UserDescriptionRepository;
-    private userStatsRepository: UserStatsRepository;
-    private userTokenRepository: UserTokenRepository;
+export class UserRepository {
+  private userActionsRepository: UserActionsRepository;
+  private userActivityRepository: UserActivityRepository;
+  private userDescriptionRepository: UserDescriptionRepository;
+  private userStatsRepository: UserStatsRepository;
+  private userTokenRepository: UserTokenRepository;
 
-    constructor(userModel: Model<IUser>) {
-        this.userActionsRepository = new UserActionsRepository(userModel);
-        this.userActivityRepository = new UserActivityRepository(userModel);
-        this.userDescriptionRepository = new UserDescriptionRepository(userModel);
-        this.userStatsRepository = new UserStatsRepository(userModel);
-        this.userTokenRepository = new UserTokenRepository(userModel);
-    }
+  constructor(userModel: Model<IUser>) {
+    this.userActionsRepository = new UserActionsRepository(userModel);
+    this.userActivityRepository = new UserActivityRepository(userModel);
+    this.userDescriptionRepository = new UserDescriptionRepository(userModel);
+    this.userStatsRepository = new UserStatsRepository(userModel);
+    this.userTokenRepository = new UserTokenRepository(userModel);
+  }
 
-    getUserActionsRepository(): UserActionsRepository {
-        return this.userActionsRepository;
-    }
+  getUserActionsRepository(): UserActionsRepository {
+    return this.userActionsRepository;
+  }
 
-    getUserActivityRepository(): UserActivityRepository {
-        return this.userActivityRepository;
-    }
+  getUserActivityRepository(): UserActivityRepository {
+    return this.userActivityRepository;
+  }
 
-    getUserDescriptionRepository(): UserDescriptionRepository {
-        return this.userDescriptionRepository;
-    }
+  getUserDescriptionRepository(): UserDescriptionRepository {
+    return this.userDescriptionRepository;
+  }
 
-    getUserStatsRepository(): UserStatsRepository {
-        return this.userStatsRepository;
-    }
+  getUserStatsRepository(): UserStatsRepository {
+    return this.userStatsRepository;
+  }
 
-    getUserTokenRepository(): UserTokenRepository {
-        return this.userTokenRepository;
+  getUserTokenRepository(): UserTokenRepository {
+    return this.userTokenRepository;
+  }
+
+  // Agregar método estático para obtener la instancia única
+  private static instance: UserRepository;
+
+  static getInstance(userModel: Model<IUser>): UserRepository {
+    if (!UserRepository.instance) {
+      UserRepository.instance = new UserRepository(userModel);
     }
-    }
+    return UserRepository.instance;
+  }
+
+}
+
+export const GlobalUserRepository = UserRepository.getInstance(UserModel);
