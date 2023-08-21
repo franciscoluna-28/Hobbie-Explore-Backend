ed6975b31f1ce8f7f5e42723f1159175a2973683
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-08-21 15:26:27 -0400

    add functionality to change description

--- src/api/user/userRouter.ts
+++ src/api/user/userRouter.ts
@@ -1,58 +1,57 @@
-  import { UserActivitiesController } from './controller/userActivitiesController';
-  import { Router } from "express";
-  import { UserActionsController } from "./controller/userActionsController";
-  import { UserStatsController } from './controller/userStatsController';
-  import UserModel from "./userModel";
+import { UserActivitiesController } from "./controller/userActivitiesController";
+import { Router } from "express";
+import { UserActionsController } from "./controller/userActionsController";
+import { UserStatsController } from "./controller/userStatsController";
+import UserModel from "./userModel";
+import { UserDescriptionController } from "./controller/userDescriptionController";
 
-  const userRouter = Router();
+// Instance Controller
+const userRouter = Router();
 
-  // Creating a single instance to the user actions controller
-  const userActionsController = UserActionsController.getInstance();
-  const userActivitiesController = UserActivitiesController.getInstance(UserModel);
-  const userStatsController = new UserStatsController(UserModel);
+// Creating a single instance to the user actions controller
+const userActionsController = UserActionsController.getInstance();
+const userActivitiesController =
+  UserActivitiesController.getInstance(UserModel);
+const userStatsController = new UserStatsController(UserModel);
+const userDescriptionController = new UserDescriptionController();
 
-  // User - actions routes
-  userRouter.post("/register", userActionsController.create);
-  userRouter.delete("/delete/:uid", userActionsController.delete);
+// User - actions routes
+userRouter.post("/register", userActionsController.create);
+userRouter.delete("/delete/:uid", userActionsController.delete);
 
-  // User - activities routes
-  userRouter.get("/default-activities/:uid", userActivitiesController.getUserHobbyExploreActivities);  
-  userRouter.delete("/delete-default-activity/:uid", userActivitiesController.deleteActivityFromUser);
-  userRouter.post("/save-default-activity/:uid", userActivitiesController.addActivityToUser);
-  userRouter.get("/is-saved/:uid", userActivitiesController.checkIfActivityIsSaved);
-
-  // User - stats routes
-  userRouter.get("/global-favorite-categories/:uid", userStatsController.getFavoriteCategories);
-  userRouter.get("/global-stats/:uid", userStatsController.getUserTotalStats);
-
-  export default userRouter;
-
-
-
-
-
-
-
-/* userRouter.post("/register-user-token/:uid", routerController.addTokenToUser);
-userRouter.post(
-  "/add-activity-to-user/:uid",
-  routerController.addActivityToUser
+// User - activities routes
+userRouter.get(
+  "/default-activities/:uid",
+  userActivitiesController.getUserHobbyExploreActivities
 );
 userRouter.delete(
-  "/remove-activity-from-user/:uid",
-  routerController.deleteActivityFromUser
+  "/delete-default-activity/:uid",
+  userActivitiesController.deleteActivityFromUser
+);
+userRouter.post(
+  "/save-default-activity/:uid",
+  userActivitiesController.addActivityToUser
 );
 userRouter.get(
-  "/get-activities-by-user/:uid",
-  routerController.getCurrentUserActivities
+  "/is-saved/:uid",
+  userActivitiesController.checkIfActivityIsSaved
 );
 
-userRouter.get("/get-activities-ids/:uid", routerController.getSavedActivitiesIds)
-userRouter.get("/get-favority-categories/:uid", routerController.getFavorityCategories)
-userRouter.post("/update-description/:uid", routerController.updateUserDescription)
-userRouter.get("/get-description/:uid", routerController.getUserDescription) */
+// User - stats routes
+userRouter.get(
+  "/global-favorite-categories/:uid",
+  userStatsController.getFavoriteCategories
+);
+userRouter.get("/global-stats/:uid", userStatsController.getUserTotalStats);
 
-// TODO 
-/* Skip watch and learn as keywords
- */
+// User - description routes
+userRouter.get(
+  "/description/:uid",
+  userDescriptionController.getUserDescription
+);
+userRouter.patch(
+  "/description/:uid",
+  userDescriptionController.updateUserDescription
+);
 
+export default userRouter;
