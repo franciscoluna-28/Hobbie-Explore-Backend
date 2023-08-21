2f9360873da22b45e7be13fa6a7cf9edd9f1c33c
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-08-21 15:26:03 -0400

    create controller to handle the user description

---
+++ src/api/user/controller/userDescriptionController.ts
@@ -0,0 +1,39 @@
+import { Request, Response } from "express";
+import { UserDescriptionRepository } from "../repository/userDescriptionRepository";
+import { UserRepository } from "../repository/userRepository";
+import UserModel from "../userModel";
+import { IUser } from "../../../types/userTypes";
+import { GlobalUserRepository } from "../repository/userRepository";
+
+export class UserDescriptionController {
+  private userDescriptionRepository: UserDescriptionRepository;
+  private userRepository: UserRepository;
+
+  constructor() {
+    this.userRepository = GlobalUserRepository;
+    this.userDescriptionRepository = this.userRepository.getUserDescriptionRepository();
+  }
+
+  updateUserDescription = async (req: Request, res: Response) => {
+    const { uid } = req.params;
+    const { newText } = req.body;
+
+    try {
+      const updatedUser = await this.userDescriptionRepository.updateUserDescription(uid, newText);
+      return res.status(200).json(updatedUser);
+    } catch (error: any) {
+      return res.status(500).json({ error: error.message });
+    }
+  }
+
+  getUserDescription = async (req: Request, res: Response) => {
+    const { uid } = req.params;
+
+    try {
+      const description = await this.userDescriptionRepository.getUserDescription(uid);
+      return res.status(200).json({ description });
+    } catch (error: any) {
+      return res.status(500).json({ error: error.message });
+    }
+  }
+}
