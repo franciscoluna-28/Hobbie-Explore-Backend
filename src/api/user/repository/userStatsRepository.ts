819ffe5f79678ac1a390292d0344bbe1bcce54c5
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-08-29 08:58:15 -0400

    update imports

--- src/api/user/repository/userStatsRepository.ts
+++ src/api/user/repository/userStatsRepository.ts
@@ -5,7 +5,7 @@ import {
   getTotalActivitiesForUser,
 } from "../../../helpers/userRepositoryHelper";
 import { IPredefinedActivityIDArray } from "../../../types/activityTypes";
-import ActivityModel from "../../activity/activityModel";
+import ActivityModel from "../../activity/default-activity-model";
 import CommentModel from "../../comment/commentModel";
 import { CategoryCounts } from "../../../types/statsType";
 
@@ -21,13 +21,13 @@ export class UserStatsRepository {
       if (!currentUser) {
         throw new Error("User not found");
       }
-  
+
       const currentUserActivities = currentUser.savedDefaultActivities;
-  
+
       const activities = await ActivityModel.find({
         id: { $in: currentUserActivities },
       });
-  
+
       const categoryCounts: CategoryCounts = activities.reduce(
         (counts, activity) => {
           const { type } = activity;
@@ -36,19 +36,20 @@ export class UserStatsRepository {
         },
         {} as CategoryCounts
       );
-  
+
       const favoriteCategories = Object.entries(categoryCounts).map(
         ([category, count]) => ({ category, count })
       );
-  
+
       return favoriteCategories; // This is an array of objects
     } catch (error) {
       console.error("Error retrieving user favorite categories:", error);
-      throw new Error("An error occurred while retrieving user favorite categories");
+      throw new Error(
+        "An error occurred while retrieving user favorite categories"
+      );
     }
   }
-  
-  
+
   // Gets the user current stats within the global app
   async getUserTotalStats(uid: string) {
     try {
