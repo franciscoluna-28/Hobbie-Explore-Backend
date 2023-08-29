566e48b732dd617603c92b91776ec0ed779c9cff
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-08-29 08:55:08 -0400

    create default activity model and router

---
+++ src/api/activity/default-activity-model.ts
@@ -0,0 +1,99 @@
+import { Schema, Document, PaginateModel } from "mongoose";
+import mongoose from "mongoose";
+import paginate from "mongoose-paginate-v2";
+import { IPredefinedActivity } from "../../types/activityTypes";
+
+// Esquema de la actividad con imagen
+const DefaultActivitySchema = new Schema<IPredefinedActivity>({
+  name: {
+    type: String,
+    required: true,
+  },
+  id: {
+    type: String,
+    required: true,
+  },
+  imageId: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: String,
+    required: true,
+  },
+  participants: {
+    type: Number,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  accessibility: {
+    type: Number,
+    required: true,
+  },
+  description: {
+    type: String,
+    default: "",
+  },
+  averageRating: {
+    type: Number,
+    default: 0,
+  },
+  reviews: {
+    type: Number,
+    default: 0,
+  },
+  blur_hash: {
+    type: String,
+    required: true,
+  },
+  listOfLinks: [
+    {
+      type: String,
+      default: [],
+    },
+  ],
+  urls: {
+    full: {
+      type: String,
+      required: true,
+    },
+    regular: {
+      type: String,
+      required: true,
+    },
+  },
+  user: {
+    name: {
+      type: String,
+      required: true,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    links: {
+      html: {
+        type: String,
+        required: true,
+      },
+    },
+    profile_image: {
+      medium: {
+        type: String,
+        required: true,
+      },
+    },
+  },
+});
+
+DefaultActivitySchema.plugin(paginate);
+
+const DefaultActivityModel = mongoose.model<
+  IPredefinedActivity & Document,
+  mongoose.PaginateModel<IPredefinedActivity & Document>
+>("DefaultActivity", DefaultActivitySchema);
+
+export default DefaultActivityModel;
---
+++ src/api/activity/default-activity-router.ts
@@ -0,0 +1,50 @@
+import { Router } from "express";
+import { DefaultActivityFromDBController } from "./pre-defined-activity/controller/default-activity-from-db-controller";
+import { DefaultActivityController } from "./pre-defined-activity/controller/default-activity-random-controller";
+import { downloadImageHandler } from "../../utils/download-image-handler";
+import { DefaultActivitiesUserController } from "./pre-defined-activity/controller/default-activity-user-actions-controller";
+
+const defaultActivityRouter = Router();
+const predefinedActivitiesDBController = new DefaultActivityFromDBController();
+const predefinedActivitiesController = new DefaultActivityController();
+const defaultUserActivitiesController = new DefaultActivitiesUserController();
+
+// Random activities
+defaultActivityRouter.get(
+  "/random",
+  predefinedActivitiesController.getThreeRandomActivities
+);
+defaultActivityRouter.get(
+  "/random/:type",
+  predefinedActivitiesController.getThreeRandomActivitiesWithQuery
+);
+
+defaultActivityRouter.get(
+  "/three-activities-from-db-with-type",
+  predefinedActivitiesDBController.recommendThreeRandomDefaultDBActivities
+);
+
+// Helper method to get one activity with its id
+defaultActivityRouter.get(
+  "/activity-by-id/:id",
+  predefinedActivitiesDBController.getActivityById
+);
+
+// Helper method to donwload the image of one activity
+defaultActivityRouter.get(
+  "/download-image-from-activity",
+  downloadImageHandler
+);
+
+// User - activities methods
+defaultActivityRouter.get(
+  "/user-activities/:uid",
+  defaultUserActivitiesController.getUserHobbyExploreActivities
+);
+
+defaultActivityRouter.post(
+  "/save-default-activity/:uid",
+  defaultUserActivitiesController.addActivityToUser
+);
+
+export default defaultActivityRouter;
