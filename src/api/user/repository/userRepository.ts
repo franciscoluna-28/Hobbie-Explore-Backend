85015213c86f64e99c226366f3ad9748c0f416d3
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-08-31 11:49:45 -0400

    remove user activity repository

--- src/api/user/repository/userRepository.ts
+++ src/api/user/repository/userRepository.ts
@@ -1,5 +1,5 @@
 import { UserActionsRepository } from "./userActionsRepository";
-import { UserActivityRepository } from "./userActivityRepository";
+
 import { UserDescriptionRepository } from "./userDescriptionRepository";
 import { UserStatsRepository } from "./userStatsRepository";
 import { UserTokenRepository } from "./userTokenRepository";
@@ -9,14 +9,12 @@ import UserModel from "../userModel";
 
 export class UserRepository {
   private userActionsRepository: UserActionsRepository;
-  private userActivityRepository: UserActivityRepository;
   private userDescriptionRepository: UserDescriptionRepository;
   private userStatsRepository: UserStatsRepository;
   private userTokenRepository: UserTokenRepository;
 
   constructor(userModel: Model<IUser>) {
     this.userActionsRepository = new UserActionsRepository(userModel);
-    this.userActivityRepository = new UserActivityRepository(userModel);
     this.userDescriptionRepository = new UserDescriptionRepository(userModel);
     this.userStatsRepository = new UserStatsRepository(userModel);
     this.userTokenRepository = new UserTokenRepository(userModel);
@@ -24,10 +22,6 @@ export class UserRepository {
 
   getUserActionsRepository(): UserActionsRepository {
     return this.userActionsRepository;
-  }
-
-  getUserActivityRepository(): UserActivityRepository {
-    return this.userActivityRepository;
   }
 
   getUserDescriptionRepository(): UserDescriptionRepository {
