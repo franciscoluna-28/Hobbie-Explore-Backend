a94397dab2d8897f2263a2e21913ade8e549cf9e
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-09-03 12:22:22 -0400

    change method in average rating from post to get

--- src/api/rating/ratingRouter.ts
+++ src/api/rating/ratingRouter.ts
@@ -7,6 +7,6 @@ const ratingController = new RatingController();
 
 ratingRouter.post("/rate-activity", ratingController.rateActivity)
 ratingRouter.get("/current-rating-in-activity/:uid/:activityId", ratingController.getCurrentUserRating)
-ratingRouter.post("/get-reviews-and-average-rating", ratingController.getReviewsAndAverageRating)
+ratingRouter.get("/get-reviews-and-average-rating", ratingController.getReviewsAndAverageRating)
 
 export default ratingRouter;
