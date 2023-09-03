2c3b881168e8bd1b259592519d87113c8bdf865c
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-09-03 12:24:58 -0400

    .

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,16 @@
 import app from ".";
 const port = process.env.SERVER_PORT || 3000;
 import { db } from "./db"; // Importar la conexión a la base de datos desde el archivo db.ts
-import { updateActivitiesWithDescription } from "./utils/addDescriptionAndLinksToActivities";
-import UserModel from "./api/user/userModel";
 
 // Starting the server
 db.once("open", () => {
   console.log(`Successfully connected to database: ${db.name}`);
+
   app.listen(port, () => {
     console.log(`Server listening at port ${port}`);
   
+
+
   });
 });
 
