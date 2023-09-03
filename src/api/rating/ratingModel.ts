f98d1ae09d2ef085495852077d6642d08bcee82b
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-09-03 12:22:37 -0400

    update rating schema to handle user UID

--- src/api/rating/ratingModel.ts
+++ src/api/rating/ratingModel.ts
@@ -2,14 +2,14 @@ import { Schema, Document, model } from "mongoose";
 
 // Interfaz para el modelo de ratings
 export interface IUserRating extends Document {
-  userId: Schema.Types.ObjectId;
+  userUID: string;
   rating: number;
   activityId: string;
 }
 
 const ratingSchema = new Schema<IUserRating>({
-  userId: {
-    type: Schema.Types.ObjectId,
+  userUID: {
+    type: String,
     ref: "User",
     required: true,
   },
